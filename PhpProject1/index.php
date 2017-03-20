baadfe79e286d140a09a82574619a3cc3e8c0e2d
vei tsi <xtfkpi@gmail.com>
2017-03-20 10:35:14 +0200

    initial commit2

---
+++ PhpProject1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'Car.php';
+        echo 2+2;
+        $v=new Car();
+        ?>
+    </body>
+</html>
